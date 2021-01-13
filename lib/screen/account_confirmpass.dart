4bc25f44aa5fae02350cce78baef767420adaa31
arissaputra362 <arissaputra362@gmail.com>
2021-01-13 22:38:06 +0700

    fix bug navbar ilang

--- lib/screen/account_confirmpass.dart
+++ lib/screen/account_confirmpass.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:decader/screen/account_screen.dart';
+import 'package:decader/screen/home.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -219,7 +220,7 @@ class _ConfirmState extends State<Confirmpass> {
                                                       context,
                                                       new MaterialPageRoute(
                                                           builder: (context) =>
-                                                              AccountScreen()));
+                                                              Home()));
                                                 },
                                                 child: Text(
                                                   'Batalkan',
